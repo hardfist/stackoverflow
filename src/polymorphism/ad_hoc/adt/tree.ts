65fea3fcfb27334de8cc4e966a2dd47e92b39fae
hardfist <1562502418@qq.com>
2019-09-17 09:32:40 +0800

    feat: fix tree

--- src/polymorphism/ad_hoc/adt/tree.ts
+++ src/polymorphism/ad_hoc/adt/tree.ts
@@ -1,37 +1,52 @@
 
 
-type Tree = TreeNode | Empty;
+type Tree = TreeNode | Empty | Leaf;
 type Empty = {
   kind: 'empty'
 }
 type TreeNode = {
   kind: 'node',
   left: Tree,
+  val: number;
   right: Tree
+}
+type Leaf = {
+  kind: 'leaf',
+  val: number
+}
+
+export function depth(tree: Tree):number{
+  switch(tree.kind){
+    case 'empty': return 0;
+    case 'node': return 1 + Math.max(depth(tree.left) + depth(tree.right))
+    case 'leaf': return 1;
+  }
+}
+export function sum(tree: Tree): number{
+  switch(tree.kind){
+    case 'empty': return 0;
+    case 'node': return tree.val + sum(tree.left) + sum(tree.right)
+    case 'leaf': return tree.val
+  }
 }
 const root: Tree = {
   kind: 'node',
+  val: 1,
   left: {
     kind: 'node', 
+    val: 2,
     left: {
-      kind: 'empty'
+      kind: 'leaf',
+      val: 3
     },
     right: {
-      kind: 'empty'
+      kind: 'leaf',
+      val:4
     }
   },
   right: {
     kind: 'empty'
   }
 }
-
-export function depth(tree: Tree):number{
-  if(tree.kind === 'empty'){
-    return 0;
-  }else{
-    return 1 + Math.max(depth(tree.left), depth(tree.right))
-  }
-}
-
-
 console.log(depth(root))
+console.log(sum(root))
